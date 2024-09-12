002fe44b60d5baa9ca85d00d86bcb30654dfe596
Sung Yeob Jung <sung87331@gmail.com>
2024-09-13 02:09:59 +0900

    ex8 1차 완성

--- FEBasic/정성엽/ex8.ts
+++ FEBasic/정성엽/ex8.ts
@@ -1,11 +1,37 @@
-// dummy(mock)입니다. 올바르게 수정하세요.
-const debounce = (cb: any, delay: number) => (i: number) => {};
-const throttle = (cb: any, delay: number) => (i: number) => {};
+/** 공통적으로 사용하는 Timer를 mode에 따라 다르게 */
+const createTimer = <T extends (...args: Parameters<T>) => ReturnType<T>>(
+    cb: T,
+    delay: number,
+    mode: "debounce" | "throttle"
+): ((...args: Parameters<T>) => void) => {
+    let timer: NodeJS.Timeout | null = null;
+    return (...args: Parameters<T>): void => {
+        if (mode === "throttle" && timer) return;
+        if (timer) clearTimeout(timer);
+        timer = setTimeout(() => {
+            cb(...args);
+            timer = null;
+        }, delay);
+    };
+};
+/** debounce 함수 */
+const debounce = <T extends (...args: Parameters<T>) => ReturnType<T>>(
+    cb: T,
+    delay: number
+) => createTimer(cb, delay, "debounce");
 
-// function throttle...
+/** throttle 함수 */
+const throttle = <T extends (...args: Parameters<T>) => ReturnType<T>>(
+    cb: T,
+    delay: number
+) => createTimer(cb, delay, "throttle");
 
-const debo = debounce((a: number) => console.log(a + 1), 500);
+/** debounce 호출 */
+const debo = debounce((a: number) => console.log("debounce: ",a + 1), 500);
 for (let i = 10; i < 15; i++) debo(i); // 15 출력
 
-const thro = throttle((a: number) => console.log(a + 1), 500);
+/** throttle 호출 */
+const thro = throttle((a: number) => console.log("throttle: ", a + 1), 500);
 for (let i = 10; i < 15; i++) thro(i); // 11 출력
+
+export = { debounce, throttle };
