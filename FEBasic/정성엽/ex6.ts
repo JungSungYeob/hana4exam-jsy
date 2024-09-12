30cd4dedc58007e4f7bf22ca9cd34643c52cb8a5
Sung Yeob Jung <sung87331@gmail.com>
2024-09-13 02:09:21 +0900

    ex6 1차 완성

--- FEBasic/정성엽/ex6.ts
+++ FEBasic/정성엽/ex6.ts
@@ -1,4 +1,30 @@
 export const randTime = <T>(val: T): Promise<T> =>
-  new Promise(resolve => setTimeout(resolve, Math.random() * 1000, val));
+    new Promise((resolve) => setTimeout(resolve, Math.random() * 1000, val));
 
-export function promiseAllSettled<T>(promises: Promise<T>[]) {}
+export function promiseAllSettled<T>(
+    promises: Promise<T>[]
+): Promise<{ status: "fulfilled" | "rejected"; value?: T; reason?: string }[]> {
+    return new Promise((resolve) => {
+        const results: {
+            status: "fulfilled" | "rejected";
+            value?: T;
+            reason?: string;
+        }[] = [];
+        let promiseCnt = promises.length;
+
+        promises.forEach((promise, index) => {
+            promise
+                .then((value) => {
+                    results[index] = { status: "fulfilled", value };
+                })
+                .catch((reason) => {
+                    results[index] = { status: "rejected", reason };
+                })
+                .finally(() => {
+                    if ((promiseCnt-=1) === 0) {
+                        resolve(results);
+                    }
+                });
+        });
+    });
+}
