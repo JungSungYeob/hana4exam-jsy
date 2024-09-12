4d55efe8a28801b0a18f6a3de4b8216bf936f9b6
Sung Yeob Jung <sung87331@gmail.com>
2024-09-13 02:09:39 +0900

    ex7 1차 완성

--- FEBasic/정성엽/ex7.ts
+++ FEBasic/정성엽/ex7.ts
@@ -1,3 +1,44 @@
-const POST_URL = 'https://jsonplaceholder.typicode.com/posts';
+const POST_URL = "https://jsonplaceholder.typicode.com/posts";
 
-export async function getPosts(userId: number | string) {}
+interface Comment {
+    postId: number;
+    id: number;
+    email: string;
+    body: string;
+}
+
+interface Post {
+    postId: number;
+    title: string;
+    comments: Comment[];
+}
+
+export async function getPosts(userId: number | string): Promise<Post[]> {
+    const postsResponse = await fetch(`${POST_URL}?userId=${userId}`);
+    const postsData = (await postsResponse.json()) as Post[];
+    // console.log("🚀 ~ getPosts ~ postsData:", postsData);
+
+    const postsWithComments = await Promise.all(
+        postsData.map(async (post: any) => {
+            const commentsResponse = await fetch(
+                `${POST_URL}/${post.id}/comments`
+            );
+            const commentsData = (await commentsResponse.json()) as Comment[];
+
+            const filteredComments = commentsData.map((comment) => ({
+                postId: comment.postId,
+                id: comment.id,
+                email: comment.email,
+                body: comment.body,
+            }));
+
+            return {
+                postId: post.id,
+                title: post.title,
+                comments: filteredComments,
+            };
+        })
+    );
+
+    return postsWithComments;
+}
